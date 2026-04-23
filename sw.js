ee553de74bbca431d898de8fd75def5702a18eb4
gousigavs <gousigavs@gmail.com>
2026-04-23 21:50:13 +0530

    Fix service worker cache - force update

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='lt-v1';
+const CACHE='lt-v2';
 const ASSETS=['./life-tracker.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
